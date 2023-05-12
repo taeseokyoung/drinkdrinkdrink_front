c97e0ec1aa41f5ab4bceeada7500c233911493d4
Raoul <nasj1621@gmail.com>
2023-05-12 16:54:10 +0900

    🐛Fix: 별점, 사진 업로드  기능 수정 #4
    23.05.12
    doc/post.html : 업로드 이미지 미리보기 추가 / 별점 선택까지 확인
    js/post.js : 별점 range에서 select로 변경

--- js/post.js
+++ js/post.js
@@ -2,9 +2,32 @@ window.onload = ()=>{
     console.log("로딩!")
 }
 
+// 업로드 이미지 미리보기
+function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        document.getElementById('preview').src = e.target.result;
+      };
+      reader.readAsDataURL(input.files[0]);
+    } else {
+      document.getElementById('preview').src = "";
+    }
+  }
+
+// function selectStar(){
+//     const viewStars = document.getElementById("stars");
+//     const stars = viewStars.option[viewStars.selectedIndex].value;
+
+// }
+
 async function handlePost(){
-    const title = document.getElementById("title")
-    const comment = document.getElementById("comment")
+    const title = document.getElementById("title");
+    const comment = document.getElementById("comment");
+    const image = document.getElementById("image");
+    const viewStars = document.getElementById("stars");
+    const stars = viewStars.options[viewStars.selectedIndex].value;
+
     
     if (title.value && comment.value){
         const response = await fetch('http://127.0.0.1:8000/write/', {
@@ -14,11 +37,14 @@ async function handlePost(){
         method:'POST',
         body: JSON.stringify({
             "title":title.value,
-            "comment":comment.value
+            "comment":comment.value,
+            // "image":image.value,
+            "starts":stars
         })
             
         })
-        console.log(title.value, comment.value)
+        console.log(title.value, comment.value, stars) 
+        //
         title.value = ""
         comment.value = ""
     } else {
@@ -28,3 +54,7 @@ async function handlePost(){
 
     
 }
+
+
+
+
