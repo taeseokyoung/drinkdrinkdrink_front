834780bca12d6cf0f5b4bbc5daee6563207a5044
Seman_Kim <sdoramkim@gmail.com>
2023-05-12 22:05:01 +0900

    :feat:Feat: 데이터 전송 방식 form-data로 변경 #2
    23.05.12
    signup.html : 주량, 주종, 이미지 추가
    signup.js : 이미지파일 추가, form-data로 이미지도 함께 전송
    
    이미지 선택시 html에서 미리보기 띄워주기
    alert 구현하기
    비밀번호 확인 만들기

--- js/signup.js
+++ js/signup.js
@@ -1,20 +1,35 @@
-async function handleSignin() {
+const frontend_base_url = "http://127.0.0.1:5500"
+const backend_base_url = "http://127.0.0.1:8000"
+
+async function handleSignup() {
     const email = document.getElementById("email").value
     const user_id = document.getElementById("user_id").value
     const nickname = document.getElementById("nickname").value
     const password = document.getElementById("password").value
-    console.log(email, user_id, password)
-    const response = await fetch('http://127.0.0.1:8000/users/signup/', {
-        headers: {
-            'content-type': 'application/json',
-        },
+    const age = document.getElementById("age").value
+    const profile_img = document.getElementById("profile_img").files[0]
+    const fav_alcohol = document.getElementById("fav_alcohol").value
+    const amo_alcohol = document.getElementById("amo_alcohol").value
+    const formdata = new FormData();
+
+    formdata.append('email', email)
+    formdata.append('user_id', user_id)
+    formdata.append('nickname', nickname)
+    formdata.append('password', password)
+    formdata.append('age', age)
+    formdata.append('profile_img', profile_img)
+    formdata.append('fav_alcohol', fav_alcohol)
+    formdata.append('amo_alcohol', amo_alcohol)
+    console.log(formdata)
+    const response = await fetch(`${backend_base_url}/users/signup/`, {
         method: 'POST',
-        body: JSON.stringify({
-            "email": email,
-            "password": password,
-            "user_id": user_id,
-            "nickname": nickname
-        })
-    })
+        body: formdata
+    }
+    )
+
+    if (response.status == 201) {
+        alert("회원가입을 축하합니다!")
+        window.location.replace(`${frontend_base_url}/doc/signin.html`)
+    }
     console.log(response)
 }
