32f6790da82894d32dc7fbddae78f97c8b517c39
0v0baek <qorqor4592@gmail.com>
2023-05-13 18:59:56 +0900

    :sparkles:Feat: 좋아요, 팔로우 순 정렬 버튼 추가 #1
    23.05.13.
    js/api.js : 버튼 누를 시 좋아요/팔로우/최신순으로 정렬되는 function 추가
    index.html : 버튼 추가
    js/index.js : 로그인 시 글쓰기 버튼 뜨도록 변경
    css/index.css : 일부 디자인 변경

--- js/api.js
+++ js/api.js
@@ -1,5 +1,7 @@
 const backend_adress = "http://127.0.0.1:8000";
 const frontend_adress = "http://127.0.0.1:5500";
+const no_image =
+  "https://usagi-post.com/wp-content/uploads/2020/05/no-image-found-360x250-1.png";
 
 // 홈 이동
 function moveHome() {
@@ -8,7 +10,12 @@ function moveHome() {
 
 // 마이페이지 이동
 function moveProfile() {
-  window.loaction.replace(`${frontend_adress}/doc/signin.html`);
+  window.loaction.replace(`${frontend_adress}/doc/profile.html`);
+}
+
+// 글 작성 페이지 이동
+function movePost() {
+  window.loaction.replace(`${frontend_adress}/doc/post.html`);
 }
 
 // 로그인 이동
@@ -21,6 +28,8 @@ function moveSignUp() {
   window.location.replace(`${frontend_adress}/doc/signup.html`);
 }
 
+
+// 로딩 시 홈에서 아티클 가져오기
 async function getArticles() {
   const response = await fetch(`${backend_adress}/`);
 
@@ -30,4 +39,41 @@ async function getArticles() {
   } else {
     alert("불러오기 실패!");
   }
+}
+
+// 버튼 눌렀을 시 정렬
+async function handleArticles(e) {
+  let btn_id = e.id;
+  let tag = "/";
+
+  if (btn_id == "follow-btn") {
+    tag = "/?order=follow";
+  } else if (btn_id == "likes-btn") {
+    tag = "/?order=likes";
+  }
+
+  const response = await fetch(`${backend_adress}${tag}`);
+  const response_json = await response.json();
+
+  console.log(response_json);
+
+  const contentBox = document.getElementById("content-box");
+  const images = document.getElementsByClassName("img-box");
+
+  for (let i = images.length - 1; i >= 0; i--) {
+    images[i].remove();
+  }
+
+  response_json.forEach((article) => {
+    const imageBox = document.createElement("img");
+    imageBox.setAttribute("class", "img-box");
+    imageBox.setAttribute("onclick", "moveDetail()");
+
+    if (article.image) {
+      imageBox.setAttribute("src", `${backend_adress}${article.image}`);
+    } else {
+      imageBox.setAttribute("src", `${no_image}`);
+    }
+    contentBox.append(imageBox);
+  });
 }
